e4c522c09d3a0ed278d82aa0baf60708252d6324
shs-zhcsyfczyjhy <tzword@163.com>
2021-04-14 20:42:17 +0800

    集成stream的rocketmq的消费端

--- src/main/java/com/tzword/usercenter/UserCenterApplication.java
+++ src/main/java/com/tzword/usercenter/UserCenterApplication.java
@@ -2,10 +2,13 @@ package com.tzword.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan("com.tzword")
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class UserCenterApplication {
 
     public static void main(String[] args) {
