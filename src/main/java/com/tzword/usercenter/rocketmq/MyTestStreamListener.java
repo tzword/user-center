738d316b64b54e945c9259c3a877b6d8a7d60db5
shs-zhcsyfczyjhy <tzword@163.com>
2021-04-16 11:53:24 +0800

    集成stream的添加异常

--- src/main/java/com/tzword/usercenter/rocketmq/MyTestStreamListener.java
+++ src/main/java/com/tzword/usercenter/rocketmq/MyTestStreamListener.java
@@ -3,6 +3,8 @@ package com.tzword.usercenter.rocketmq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +18,12 @@ public class MyTestStreamListener {
     @StreamListener(MySink.MY_INPUT)
     public void receive(String messageBody){
         log.info("通过自定义的stream收到了消息：messageBody:{}",messageBody);
+        throw new IllegalArgumentException("发生异常");
+    }
+
+    @StreamListener("errorChannel")
+    public void error(Message<?> message){
+        ErrorMessage errorMessage = (ErrorMessage) message;
+        log.error("发生异常了：{}",errorMessage);
     }
 }
